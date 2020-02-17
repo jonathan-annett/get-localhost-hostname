65e28eb78e1c87814a78618b683f378799113b2a
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-18 08:43:54 +1100

    updated for wsl

--- start-browser.js
+++ start-browser.js
@@ -1,25 +1,47 @@
+var detect_os_name = require("./detect_os_name"); 
+var this_os=detect_os_name();
+var hostname=detect_os_name.hostname;
 
-var 
+var child_process = require("child_process") ;
+var spawnSync = child_process.spawnSync;
+var spawn = child_process.spawn;
 
-hostname=require(".");
-
-function open_browser(url){
-    var start = require("os").platform()==="linux" ? "xdg-open" : "cmd.exe";
-    var args = start === "xdg-open" ? [url] : ["/C","start", url];
-    require("child_process")
-        .spawn(start,args)
-           .on('error',function(){
-               console.log("(could not auto start browser)")
-           });
+function open_browser_generic(start,args){
+    spawn(start,args).on('error',function(e){
+       console.log("(could not auto start browser - "+e.toString()+")");
+    });
 }
 
+function open_browser_chromeos(url){
+    var start = "xdg-open";
+    var args = [url] ;
+    open_browser_generic(start,args);
+}
+
+function open_browser_linux(url){
+    var start = "xdg-open";
+    var args = [url] ;
+    open_browser_generic(start,args);
+}
+ 
+function open_browser_wsl(url){
+    var start = open_browser_wsl.which || (open_browser_wsl.which=child_process.spawnSync("which",["cmd.exe"]).output[1].toString().trim());
+    var args = ["/C","start", url];
+    open_browser_generic(start,args);
+}
+ 
 module.exports = function(app,port,uri) {
     var listener = app.listen(port||0,function(){
         var url="http://"+hostname+":"+listener.address().port+(uri?uri:'/');
         console.log("goto to "+url);
-        open_browser(url);
+        module.exports.open_browser(url);
     });
 };
 
 module.exports.hostname     = hostname;
-module.exports.open_browser = open_browser;
+switch(this_os) {
+    case "wsl"      : module.exports.open_browser = open_browser_wsl; break; 
+    case "chromeos" : module.exports.open_browser = open_browser_chromeos; break;
+    case "linux"    : module.exports.open_browser = open_browser_linux; break;
+    default         : module.exports.open_browser = function(){console.log("(could not auto start browser - your os ("+this_os+") is not supported)");};
+}
