0918fd36bb897bc28d1ba10416d37efb4a411813
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-18 07:32:01 +1100

    exposed open_browser(url) as seperate function, and hostname as string

--- start-browser.js
+++ start-browser.js
@@ -1,20 +1,25 @@
 
 var 
 
-hostname=require("."),
-lookup_os_linux_name   = require("./detect_os_name");
+hostname=require(".");
+
+function open_browser(url){
+    var start = require("os").platform()==="linux" ? "xdg-open" : "cmd.exe";
+    var args = start === "xdg-open" ? [url] : ["/C","start", url];
+    require("child_process")
+        .spawn(start,args)
+           .on('error',function(){
+               console.log("(could not auto start browser)")
+           });
+}
 
 module.exports = function(app,port,uri) {
     var listener = app.listen(port||0,function(){
         var url="http://"+hostname+":"+listener.address().port+(uri?uri:'/');
         console.log("goto to "+url);
-        var browser = (lookup_os_linux_name()==='wsl') ? "cmd.exe" : "xdg-open" ;
-        var args = browser === "xdg-open" ? [url] : ["/C","start", url];
-        require("child_process")
-            .spawn(browser,args)
-               .on('error',function(){
-                   console.log("(could not auto start browser)")
-               });
-            
+        open_browser(url);
     });
-}
+};
+
+module.exports.hostname     = hostname;
+module.exports.open_browser = open_browser;
