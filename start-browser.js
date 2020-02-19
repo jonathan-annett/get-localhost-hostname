5b50f5bb6f4c8327b4abecf4fc001092650bceaa
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-20 10:19:59 +1100

    added callback

--- start-browser.js
+++ start-browser.js
@@ -6,35 +6,56 @@ var child_process = require("child_process") ;
 var spawnSync = child_process.spawnSync;
 var spawn = child_process.spawn;
 
-function open_browser_generic(start,args){
-    spawn(start,args).on('error',function(e){
-       console.log("(could not auto start browser - "+e.toString()+")");
-    });
+function open_browser_generic(start,args,cb){
+    
+    var notify = typeof cb==='function' ? setTimeout(onStartup,1000): false;
+    var child = spawn(start,args);
+    child.on('error',onError);
+    function onStartup(){
+        if (!!notify) {
+            clearTimeout(notify);
+            notify=false;
+            cb(undefined,child);
+        }
+    }
+    
+    function onError(e){
+       if (notify) clearTimeout(notify);
+       var err = "(could not auto start browser - "+e.toString()+")";
+       if (notify) {
+            notify=false;
+            cb (err);
+       } else {
+           console.log(err);
+       }
+    }
+
+    return child;
 }
 
-function open_browser_chromeos(url){
+function open_browser_chromeos(url,cb){
     var start = "xdg-open";
     var args = [url] ;
-    open_browser_generic(start,args);
+    return open_browser_generic(start,args,cb);
 }
 
-function open_browser_linux(url){
+function open_browser_linux(url,cb){
     var start = "xdg-open";
     var args = [url] ;
-    open_browser_generic(start,args);
+    return open_browser_generic(start,args,cb);
 }
  
-function open_browser_wsl(url){
+function open_browser_wsl(url,cb){
     var start = open_browser_wsl.which || (open_browser_wsl.which=child_process.spawnSync("which",["cmd.exe"]).output[1].toString().trim());
     var args = ["/C","start", url];
-    open_browser_generic(start,args);
+    return open_browser_generic(start,args,cb);
 }
  
-module.exports = function(app,port,uri) {
+module.exports = function(app,port,uri,cb) {
     var listener = app.listen(port||0,function(){
         var url="http://"+hostname+":"+listener.address().port+(uri?uri:'/');
         console.log("goto to "+url);
-        module.exports.open_browser(url);
+        module.exports.open_browser(url,cb);
     });
 };
 
